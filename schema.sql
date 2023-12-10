ffd9109de172d011270e49773f2d7df08533ec47
JiyuanJia <148827858+JiyuanJia@users.noreply.github.com>
2023-12-10 15:08:08 -0500

    Update schema.sql
    manual update schemaa

--- schema.sql
+++ schema.sql
@@ -1,1 +1,37 @@
-
+Schema = """
+CREATE TABLE complaints 
+(
+    unique_key SERIAL PRIMARY KEY,
+    created_date TIMESTAMP WITHOUT TIME ZONE,
+    location_type TEXT,
+    incident_zip INTEGER,
+    geom GEOMETRY(Point, 4326)
+);
+CREATE TABLE tree
+(
+    created_date TIMESTAMP WITHOUT TIME ZONE,
+    tree_id integer,
+    status varchar(50),
+    health varchar(50),
+    problems text,
+    zipcode varchar(10),
+    state varchar(10),
+    geom GEOMETRY(Point, 4326)
+);
+CREATE TABLE rent
+(
+    RegionID integer,
+    RegionName	varchar(100),
+    date TIMESTAMP WITHOUT TIME ZONE,
+    rent float
+);
+CREATE Table zip_code
+(
+    ZIPCODE varchar(50),
+    PO_NAME varchar(50),
+    POPULATION varchar(50),
+    STATE varchar(50),
+    COUNTY  varchar(50),
+    geom GEOMETRY(Point, 4326)
+)
+"""
