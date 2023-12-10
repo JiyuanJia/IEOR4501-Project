ce884332dac12e70b12a255d46145d0f7428ac53
JiyuanJia <jiajiyuan199961@gmail.com>
2023-12-10 17:22:47 -0500

    update
    update

--- query5.sql
+++ query5.sql
@@ -2,5 +2,5 @@ SELECT z."ZIPCODE",count(tree_id) cnt FROM tree t
 JOIN public.zip_code z
 ON t.zipcode= z."ZIPCODE"::INTEGER
 GROUP BY z."ZIPCODE"
-ORDER BY count(tree_id)  desc
+ORDER BY count(tree_id) DESC
 LIMIT 10
